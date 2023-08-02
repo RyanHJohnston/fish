f60f859ab2d83341d649bfe5ae9ef21c16edeff6
ryanhjohnston <ryanhjohnstoncollege@gmail.com>
2023-08-02 17:57:18 -0500

    First commit for fish shell config

---
+++ config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fastfetch
+    alias leave="exit"
+    alias ll="ls -al --color=auto"
+    alias l="ls -l --color=auto"
+end
+
